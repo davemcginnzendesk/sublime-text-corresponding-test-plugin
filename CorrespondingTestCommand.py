44a5f70ef076288567c8a24fa316c46208bd2e31
Dave McGinn <david.mcginn@zendesk.com>
2023-05-02 10:57:34 +0100

    Changes for CorrespondingTest

--- CorrespondingTestCommand.py
+++ CorrespondingTestCommand.py
@@ -4,19 +4,19 @@ import re
 import os
 
 def find_corresponding_test(file_path):
-  file_path_parts = file_path.split('app/');
-  project_file_path = file_path_parts[1];
-  # filename = project_file_path.split('/')[-1]
-  test_filename = project_file_path.replace(".rb", "_spec.rb")
-  test_file_path = file_path_parts[0] + "spec/" + test_filename
-
-  # spec/task_adapter/consumers/task_events_consumer_spec.rb
-  return test_file_path;
+  # file_path_parts = file_path.split('app/');
+  # project_file_path = file_path_parts[1];
+  # test_filename = project_file_path.replace(".rb", "_spec.rb")
+  # test_file_path = file_path_parts[0] + "spec/" + test_filename
+  # return test_file_path;
+  return file_path.replace('app', 'spec').replace('.rb', '_spec.rb');
 
 class CorrespondingTestCommand(sublime_plugin.TextCommand):
   def run(self, edit):
     current_file = self.view.file_name();
-    # /Users/david.mcginn/Code/zendesk/voice/app/task_adapter/consumers/task_events_consumer.rb
+    if not re.search('/voice/', current_file):
+      return;
+
     test_file = find_corresponding_test(current_file);
     print(test_file);
 
